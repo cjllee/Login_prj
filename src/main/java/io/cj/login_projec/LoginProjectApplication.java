892f3d8dc7fa114a3c3639324e755e7f6f31f28b
jin <cjlee6009@gmail.com>
2025-05-24 18:30:36 +0900

    first commit prj setting

---
+++ src/main/java/io/cj/login_projec/LoginProjectApplication.java
@@ -0,0 +1,13 @@
+package io.cj.login_projec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginProjectApplication.class, args);
+	}
+
+}
