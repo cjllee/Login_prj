2df7b79152cbb35787cdebcb2e9c732e605715a3
jin <cjlee6009@gmail.com>
2025-06-08 10:05:10 +0900

    controller, config 충돌 변경 및 detailService 추가

---
+++ src/main/java/io/cj/login_project/service/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package io.cj.login_project.service;
+
+import io.cj.login_project.entity.User;
+import io.cj.login_project.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword()) // 평문 비밀번호 반환
+                .roles(user.getRole().name().replace("ROLE_", ""))
+                .build();
+    }
+}
